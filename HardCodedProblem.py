101c042340441d8ae558f1ffbe1ca88b1e05e0dc
Attillo <david.olsson.do@gmail.com>
2016-10-04 14:50:44 +0200

    Working Hard Coded

--- HardCodedProblem.py
+++ HardCodedProblem.py
@@ -6,25 +6,25 @@ Created on Mon Oct  3 13:08:13 2016
 from  scipy import *
 from  pylab import *
 
-n = 5 # Nbr of points
+n = 10 # Nbr of points
 Deltax = 1 / (n -1)
-h1 = 1
-b1 = 1
-h3 = 1
-b3 = 1
-h2 = 2
-h2l1 = h2 - h1 
-h2l2 = h1
-h2r1 = h3
-h2r2 = h2 - h3
-b2 = 1
+heigth1 = 1
+base1 = 1
+heigth3 = 1
+base3 = 1
+heigth2 = 2
+heigth2l1 = heigth2 - heigth1 
+heigth2l2 = heigth1
+heigth2r1 = heigth3
+heigth2r2 = heigth2 - heigth3
+base2 = 1
 
 
-def generateGrid(b,h, DomainTwo = False):
-    nx = n * b
-    ny = n * h
+def generateGrid(nx,ny, DomainTwo = False):
+#    nx = n * b
+#    ny = n * h
 #    Dx = self.b / (nx - 1)
-    
+#    print((ny,nx))
     pointNames = zeros((ny,nx))
     name = 0
     for i in range(0,ny):
@@ -33,9 +33,9 @@ def generateGrid(b,h, DomainTwo = False):
             name += 1
             
     if DomainTwo:
-        boundary = {'leftUpper': pointNames[0:(n*h2l1)+1,0], 'leftLower': pointNames[(n*h2l1)+1:,0], 'rightUpper': pointNames[0:(n*h2r1),-1], 'rightLower': pointNames[(n*h2r1):,-1], 'upper': pointNames[0,:], 'lower': pointNames[-1,:]}
+        boundary = {'leftUpper': pointNames[1:(n*h2l1)+1,0], 'leftLower': pointNames[(n*h2l1)+1:-1,0], 'rightUpper': pointNames[1:(n*h2r1)-1,-1], 'rightLower': pointNames[(n*h2r1)-1:-1,-1], 'upper': pointNames[0,:], 'lower': pointNames[-1,:]}
     else:
-        boundary = {'leftUpper': pointNames[:,0], 'leftLower': array([]), 'rightUpper': pointNames[:,-1], 'rightLower': array([]), 'upper': pointNames[0,:], 'lower': pointNames[-1,:]}
+        boundary = {'leftUpper': pointNames[1:-1,0], 'leftLower': array([]), 'rightUpper': pointNames[1:-1,-1], 'rightLower': array([]), 'upper': pointNames[0,:], 'lower': pointNames[-1,:]}
     
     pointNamespadded = pad(pointNames,((1,1),(1,1)),'constant',constant_values=(-1, -1))
     
@@ -49,31 +49,37 @@ def generateGrid(b,h, DomainTwo = False):
 
 
 def domainOne():
-    h = h1
-    b = b1
+    h = heigth1
+    b = base1
+    nx = 10
+    ny = 10    
     
-    conditions = {'leftUpper': ('dirichlet',40), 'leftLower': ('dirichlet',40), 'rightUpper': ('neumann',None), 'rightLower': ('neumann',None), 'upper': ('dirichlet',15), 'lower': ('dirichlet',15)}
-    edof,boundary = generateGrid(b,h)
+    conditions = {'leftUpper': ('dirichlet',array([40])), 'leftLower': ('dirichlet',array([40])), 'rightUpper': ('neumann',array([1])), 'rightLower': (None,None), 'upper': ('dirichlet',array([15])), 'lower': ('dirichlet',array([15]))}
+    edof,boundary = generateGrid(nx,ny)    
     
-    return edof,boundary,conditions,n**2*b*h
+    return edof,boundary,conditions,nx*ny,b,h
     
 def domainThree():
-    h = h3
-    b = b3
+    h = heigth3
+    b = base3
+    nx = 10
+    ny = 10  
+
+    conditions = {'leftUpper': ('neumann',array([1])), 'leftLower': (None,None), 'rightUpper': ('dirichlet',array([40])), 'rightLower': ('dirichlet',array([40])), 'upper': ('dirichlet',array([15])), 'lower': ('dirichlet',array([15]))}
+    edof,boundary = generateGrid(nx,ny)
     
-    conditions = {'leftUpper': ('neumann',None), 'leftLower': ('neumann',None), 'rightUpper': ('dirichlet',40), 'rightLower': ('dirichlet',40), 'upper': ('dirichlet',15), 'lower': ('dirichlet',15)}
-    edof,boundary = generateGrid(b,h)
-    
-    return edof,boundary,conditions,n**2*b*h
+    return edof,boundary,conditions,nx*ny,b,h
     
 def domainTwo():
-    h = h2
-    b = b2
+    h = heigth2
+    b = base2
+    nx = 10
+    ny = 19    
     
-    conditions = {'leftUpper': ('dirichlet',15), 'leftLower': ('dirichlet',15), 'rightUpper': ('dirichlet',15), 'rightLower': ('dirichlet',15), 'upper': ('dirichlet',40), 'lower': ('dirichlet',5)}
-    edof,boundary = generateGrid(b,h,True)
+    conditions = {'leftUpper': ('dirichlet',array([15])), 'leftLower': ('dirichlet',array([15])), 'rightUpper': ('dirichlet',array([15])), 'rightLower': ('dirichlet',array([15])), 'upper': ('dirichlet',array([40])), 'lower': ('dirichlet',array([5]))}
+    edof,boundary = generateGrid(nx,ny,True)
     
-    return edof,boundary,conditions,n**2*b*h
+    return edof,boundary,conditions,nx*ny,b,h
     
     
     
--- HardCodedSolver.py
+++ HardCodedSolver.py
@@ -7,9 +7,16 @@ from  scipy import *
 from  pylab import *
 import  HardCodedProblem
 
+from matplotlib import animation as anim
+from mpi4py import MPI
+
+n = 10 # Nbr of points
+Deltax = 1 / (n -1)
 
 def generateOperator(edof,boundary,conditions,nbrOfNodes):
     Operator = zeros((nbrOfNodes,nbrOfNodes))
+#    Deltax = 1 / (nbrOfNodes -1)    
+    
     for i in range(0,nbrOfNodes):
 #        if any(self.edof[i,2] == self.boundary['left']) or any(self.edof[i,2] == self.boundary['right']) or any(self.edof[i,2] == self.boundary['upper']) or any(self.edof[i,2] == self.boundary['lower']):
 #            pass
@@ -58,32 +65,153 @@ def generateOperator(edof,boundary,conditions,nbrOfNodes):
     Operator = delete(Operator, (dirichletBoundaries), axis=0)
     Operator = delete(Operator, (dirichletBoundaries), axis=1)        
     
-    return Operator
+    return Operator / Deltax**2
     
 def generateB(edof,boundary,conditions,nbrOfNodes):
     b = zeros((nbrOfNodes,))
-    Deltax = 1 / (nbrOfNodes -1)
+#    Deltax = 1 / (nbrOfNodes -1)
+    newconditions = conditions.copy()
     
+    dirichletBoundaries = []
     for key in conditions.keys():
         if (conditions[key][0] == 'neumann'):
-            b(boundary[key]) = -conditions[key][1] / Del
+            neumannPoints = array(boundary[key], dtype='int')
+#            print(neumannPoints)
+#            print(conditions[key][1])
+            b[neumannPoints] += -conditions[key][1] / Deltax
         elif (conditions[key][0] == 'dirichlet'):
+            dirichletBoundaries += list(boundary[key])
             
+            for point in boundary[key]:
+                neighbourPoints = edof[int(point),:]
+                neighbourPoints = delete(neighbourPoints, where(neighbourPoints < 0))
+                neighbourPoints = array(neighbourPoints, dtype='int')
+#                indexRemove = []                
+#                for i in range(0,len(neighbourPoints)):
+#                    if (neighbourPoints[i] < 0):
+#                        indexRemove.append(i)
+#                neighbourPoints = delete(neighbourPoints, indexRemove)
+#                print(neighbourPoints)
+#                print(key)
+                if (len(conditions[key][1]) > 1):
+                    index = where(boundary[key] == point)[0]
+#                    print(index)
+                    b[neighbourPoints] += -conditions[key][1][index] / Deltax**2 
+                else:
+                    b[neighbourPoints] += -conditions[key][1] / Deltax**2 
+                
+    dirichletBoundaries = unique(array(dirichletBoundaries))
+    dirichletBoundaries = unique(dirichletBoundaries)       
+    
+    b = delete(b, (dirichletBoundaries))
+    
+    return b,newconditions
     
 #    for i in range(0,nbrOfNodes):
 #        for point in edof[i,:]:
 #            for key in boundary.keys():
 #                if any(point == boundary[key]):
 
-
+#def generateSolutionStructure(u,boundary,conditions,nodeStructure):
     
-#edof1, boundary1, conditions1, nbrOfNodes1 = domainOne()
-#Operator1 = generateOperator(edof1, boundary1, conditions1, nbrOfNodes1)
-#edof2, boundary2, conditions2, nbrOfNodes2 = domainTwo()
-#Operator2 = generateOperator(edof2, boundary2, conditions2, nbrOfNodes2)
-edof3, boundary3, conditions3, nbrOfNodes3 = domainThree()
-Operator3 = generateOperator(edof3, boundary3, conditions3, nbrOfNodes3)
 
+edof2, boundary2, conditions2, nbrOfNodes2,base2,height2 = domainTwo()
+Operator2 = generateOperator(edof2, boundary2, conditions2, nbrOfNodes2)
+b2,_ = generateB(edof2, boundary2, conditions2, nbrOfNodes2)
+u2 = solve(Operator2, b2)
+u2 = u2.reshape((19)-2,(10)-2)
+u2old = u2
+
+domain1flow = (u2[len(boundary2['leftUpper'])-1:,0] - conditions2['leftLower'][1]) / Deltax # The value of the flow into the other domains. Positive --> other domain is heated
+domain3flow = (u2[0:len(boundary2['rightUpper']),-1] - conditions2['rightUpper'][1]) / Deltax
+
+#edof1, boundary1, conditions1, nbrOfNodes1,base1,height1 = domainOne()
+#Operator1 = generateOperator(edof1, boundary1, conditions1, nbrOfNodes1)
+#conditions1['rightUpper'] = (conditions1['rightUpper'][0], domain1flow)
+#b1,_ = generateB(edof1, boundary1, conditions1, nbrOfNodes1)
+#u1 = solve(Operator1,b1)
+#u1 = u1.reshape((10)-2,(10)-1)
+#u1old = u1
+#
+#edof3, boundary3, conditions3, nbrOfNodes3,base3,height3 = domainThree()
+#Operator3 = generateOperator(edof3, boundary3, conditions3, nbrOfNodes3)
+#conditions3['leftUpper'] = (conditions3['leftUpper'][0], domain3flow)
+#b3,_ = generateB(edof3, boundary3, conditions3, nbrOfNodes3)
+#u3 = solve(Operator3, b3)
+#u3 = u3.reshape((10)-2,(10)-1) 
+#u3old = u3
+
+omega = 0.8
+
+
+for i in range(0,100):
+    u1old = u1
+    edof1, boundary1, conditions1, nbrOfNodes1,base1,height1 = domainOne()
+    Operator1 = generateOperator(edof1, boundary1, conditions1, nbrOfNodes1)
+    conditions1['rightUpper'] = (conditions1['rightUpper'][0], domain1flow)
+    b1,_ = generateB(edof1, boundary1, conditions1, nbrOfNodes1)
+    u1 = solve(Operator1,b1)
+    u1 = u1.reshape((10)-2,(10)-1)
+    u1 = omega * u1 + (1 - omega) * u1old
+    
+    
+    u3old = u3
+    edof3, boundary3, conditions3, nbrOfNodes3,base3,height3 = domainThree()
+    Operator3 = generateOperator(edof3, boundary3, conditions3, nbrOfNodes3)
+    conditions3['leftUpper'] = (conditions3['leftUpper'][0], domain3flow)
+    b3,_ = generateB(edof3, boundary3, conditions3, nbrOfNodes3)
+    u3 = solve(Operator3, b3)
+    u3 = u3.reshape((10)-2,(10)-1) 
+    u3 = omega * u3 + (1 - omega) * u3old
+    
+    
+    u2old = u2
+    edof2, boundary2, conditions2, nbrOfNodes2,base2,height2 = domainTwo()
+    Operator2 = generateOperator(edof2, boundary2, conditions2, nbrOfNodes2)
+    conditions2['leftLower'] = (conditions2['leftLower'][0], u1[:,-1])
+    conditions2['rightUpper'] = (conditions2['rightUpper'][0], u3[:,0])
+    b2,_ = generateB(edof2, boundary2, conditions2, nbrOfNodes2)
+    u2 = solve(Operator2, b2)
+    u2 = u2.reshape((19)-2,(10)-2)
+#    matshow(u2)
+    u2 = omega * u2 + (1 - omega) * u2old
+    
+    
+    
+    domain1flow = (u2[len(boundary2['leftUpper'])-1:,0] - conditions2['leftLower'][1]) / Deltax # The value of the flow into the other domains. Positive --> other domain is heated
+    domain3flow = (u2[0:len(boundary2['rightUpper']),-1] - conditions2['rightUpper'][1]) / Deltax
+
+c1 = zeros((9,9)) + nan
+c2 = zeros((9,9)) + nan
+c1[-1,:] = 15
+c1[:,-1] = 15
+c2[:,0] = 15
+c2[0,:] = 15  
+part1 = vstack((c1,u1))
+part3 = vstack((u3,c2))
+part2 = hstack((part1,u2))
+final = hstack((part2,part3))
+final = pad(final,((1,1),(1,1)),'constant',constant_values=(nan, nan))
+final[:,0] = nan
+final[shape(final)[0]/2+1:-1,0] = 40
+final[0,:] = nan
+final[19-1,:] = nan
+final[-1,1:10] = 15
+final[-1,10:10+8] = 5
+final[1:shape(final)[0]/2,-1] = 40
+final[0,10:19-1] = 40
+final[0,19-1:-1] = 15
+fig=matshow(final)
+
+
+
+#domain1boundary = array(boundary2['leftLower'], dtype='int')
+#domain1boundary = array(edof2[domain1boundary,3], dtype='int')
+#domain1flow = (conditions2['leftLower'][1] - u2[domain1boundary]) / Deltax
+#
+#domain2boundary = array(boundary2['rightUpper'], dtype='int')
+#domain2boundary = array(edof2[domain2boundary,1], dtype='int')
+#domain2flow = (u2[domain1boundary] - conditions2['rightUpper'][1]) / Deltax
 
 
 
