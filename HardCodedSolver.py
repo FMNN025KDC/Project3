ecc535fe09f2a87351c70f06d1d92f24b42f2499
Attillo <david.olsson.do@gmail.com>
2016-10-05 14:15:15 +0200

    Empirical Programming

--- HardCodedSolver.py
+++ HardCodedSolver.py
@@ -6,11 +6,17 @@ Created on Mon Oct  3 13:44:01 2016
 from  scipy import *
 from  pylab import *
 import  HardCodedProblem
+import scipy.linalg as lin
+#from mpi4py import MPI
 
-from matplotlib import animation as anim
-from mpi4py import MPI
+n = 20
+omega = 1
+nbrOfIterations = 1000
+tolerance = 1e-5
 
-n = 10 # Nbr of points
+nx = n # Nbr of points
+ny = n
+ny2 = 2*n -1
 Deltax = 1 / (n -1)
 
 def generateOperator(edof,boundary,conditions,nbrOfNodes):
@@ -119,70 +125,121 @@ edof2, boundary2, conditions2, nbrOfNodes2,base2,height2 = domainTwo()
 Operator2 = generateOperator(edof2, boundary2, conditions2, nbrOfNodes2)
 b2,_ = generateB(edof2, boundary2, conditions2, nbrOfNodes2)
 u2 = solve(Operator2, b2)
-u2 = u2.reshape((19)-2,(10)-2)
+u2 = u2.reshape((ny2)-2,(nx)-2)
 u2old = u2
 
 domain1flow = (u2[len(boundary2['leftUpper'])-1:,0] - conditions2['leftLower'][1]) / Deltax # The value of the flow into the other domains. Positive --> other domain is heated
 domain3flow = (u2[0:len(boundary2['rightUpper']),-1] - conditions2['rightUpper'][1]) / Deltax
 
-#edof1, boundary1, conditions1, nbrOfNodes1,base1,height1 = domainOne()
-#Operator1 = generateOperator(edof1, boundary1, conditions1, nbrOfNodes1)
-#conditions1['rightUpper'] = (conditions1['rightUpper'][0], domain1flow)
-#b1,_ = generateB(edof1, boundary1, conditions1, nbrOfNodes1)
-#u1 = solve(Operator1,b1)
-#u1 = u1.reshape((10)-2,(10)-1)
-#u1old = u1
-#
-#edof3, boundary3, conditions3, nbrOfNodes3,base3,height3 = domainThree()
-#Operator3 = generateOperator(edof3, boundary3, conditions3, nbrOfNodes3)
-#conditions3['leftUpper'] = (conditions3['leftUpper'][0], domain3flow)
-#b3,_ = generateB(edof3, boundary3, conditions3, nbrOfNodes3)
-#u3 = solve(Operator3, b3)
-#u3 = u3.reshape((10)-2,(10)-1) 
-#u3old = u3
+edof1, boundary1, conditions1, nbrOfNodes1,base1,height1 = domainOne()
+Operator1 = generateOperator(edof1, boundary1, conditions1, nbrOfNodes1)
+conditions1['rightUpper'] = (conditions1['rightUpper'][0], domain1flow)
+b1,_ = generateB(edof1, boundary1, conditions1, nbrOfNodes1)
+u1 = solve(Operator1,b1)
+u1 = u1.reshape((nx)-2,(nx)-1)
+u1old = u1
 
-omega = 0.8
+edof3, boundary3, conditions3, nbrOfNodes3,base3,height3 = domainThree()
+Operator3 = generateOperator(edof3, boundary3, conditions3, nbrOfNodes3)
+conditions3['leftUpper'] = (conditions3['leftUpper'][0], domain3flow)
+b3,_ = generateB(edof3, boundary3, conditions3, nbrOfNodes3)
+u3 = solve(Operator3, b3)
+u3 = u3.reshape((nx)-2,(nx)-1) 
+u3old = u3
+#u1 = 0
+#u3 = 0
 
-
-for i in range(0,100):
+for i in range(0,nbrOfIterations):
+    print('Iteration {} of {}...'.format(i+1,nbrOfIterations))
     u1old = u1
+    conditions1old = conditions1
     edof1, boundary1, conditions1, nbrOfNodes1,base1,height1 = domainOne()
     Operator1 = generateOperator(edof1, boundary1, conditions1, nbrOfNodes1)
     conditions1['rightUpper'] = (conditions1['rightUpper'][0], domain1flow)
     b1,_ = generateB(edof1, boundary1, conditions1, nbrOfNodes1)
+    
+#    lu1 = lin.lu_factor(Operator1)
+#    u1 = lin.lu_solve(lu1, b1)
     u1 = solve(Operator1,b1)
-    u1 = u1.reshape((10)-2,(10)-1)
+    u1 = u1.reshape((nx)-2,(nx)-1)
     u1 = omega * u1 + (1 - omega) * u1old
     
     
     u3old = u3
+    conditions3old = conditions3
     edof3, boundary3, conditions3, nbrOfNodes3,base3,height3 = domainThree()
     Operator3 = generateOperator(edof3, boundary3, conditions3, nbrOfNodes3)
     conditions3['leftUpper'] = (conditions3['leftUpper'][0], domain3flow)
     b3,_ = generateB(edof3, boundary3, conditions3, nbrOfNodes3)
+    
+#    lu3 = lin.lu_factor(Operator3)
+#    u3 = lin.lu_solve(lu3, b3)
     u3 = solve(Operator3, b3)
-    u3 = u3.reshape((10)-2,(10)-1) 
+    u3 = u3.reshape((nx)-2,(nx)-1) 
     u3 = omega * u3 + (1 - omega) * u3old
     
     
     u2old = u2
+    conditions2old = conditions2
     edof2, boundary2, conditions2, nbrOfNodes2,base2,height2 = domainTwo()
     Operator2 = generateOperator(edof2, boundary2, conditions2, nbrOfNodes2)
     conditions2['leftLower'] = (conditions2['leftLower'][0], u1[:,-1])
     conditions2['rightUpper'] = (conditions2['rightUpper'][0], u3[:,0])
     b2,_ = generateB(edof2, boundary2, conditions2, nbrOfNodes2)
+    
+#    lu2 = lin.lu_factor(Operator2)
+#    u2 = lin.lu_solve(lu2, b2)    
     u2 = solve(Operator2, b2)
-    u2 = u2.reshape((19)-2,(10)-2)
+    u2 = u2.reshape((ny2)-2,(nx)-2)
 #    matshow(u2)
     u2 = omega * u2 + (1 - omega) * u2old
     
+    normDiffOld = normDiff 
+    normDiff = norm(u2 - u2old) 
+    print('normDiff: ', normDiff)
+    print('omega: ', omega)
+    omegaOld = omega
+##    if (normDiff - normDiffOld) > 0:
+##        omega = 0.8 * omega / normDiff
     
+       
+    omega = omega / (normDiff * (n/30))
+    if omega > normDiff  / (n/30):
+        omega = normDiff  / (n/30)
+#    omega = normDiff
+#    if (normDiff > normDiffOld):    
+#        omega = omegaOld * normDiffOld / 0.8
+#    if (omega > normDiff):
+#        omega = normDiff
+        
+    if (normDiff > 1e2):
+#        print(normDiff)
+#        print(normDiffOld)
+        print('Solution might not be converging.')
+        omega = 0.9 * omegaOld
+        #        print('Setting omega to: ', omega)
+        u3 = u3old
+        u2 = u2old
+        u1 = u1old
+
+        normDiff = normDiffOld        
+        
+        conditions1 = conditions1old
+        conditions2 = conditions2old
+        conditions3 = conditions3old
+    elif (normDiff < tolerance):
+        print('Solution appears to have converged within tolerance: ',tolerance)
+        break
+        omega = omega * normDiff
+#        omega = 0.01 / normDiff        
+#        print('Setting omega to: ', omega)
+     
     
     domain1flow = (u2[len(boundary2['leftUpper'])-1:,0] - conditions2['leftLower'][1]) / Deltax # The value of the flow into the other domains. Positive --> other domain is heated
     domain3flow = (u2[0:len(boundary2['rightUpper']),-1] - conditions2['rightUpper'][1]) / Deltax
 
-c1 = zeros((9,9)) + nan
-c2 = zeros((9,9)) + nan
+c1 = zeros((nx-1,nx-1)) + nan
+c2 = zeros((nx-1,nx-1)) + nan
 c1[-1,:] = 15
 c1[:,-1] = 15
 c2[:,0] = 15
@@ -192,15 +249,15 @@ part3 = vstack((u3,c2))
 part2 = hstack((part1,u2))
 final = hstack((part2,part3))
 final = pad(final,((1,1),(1,1)),'constant',constant_values=(nan, nan))
-final[:,0] = nan
-final[shape(final)[0]/2+1:-1,0] = 40
-final[0,:] = nan
-final[19-1,:] = nan
-final[-1,1:10] = 15
-final[-1,10:10+8] = 5
-final[1:shape(final)[0]/2,-1] = 40
-final[0,10:19-1] = 40
-final[0,19-1:-1] = 15
+#final[:,0] = nan
+final[nx:-1,0] = 40
+#final[0,:] = nan
+#final[ny2-1,:] = nan
+final[-1,1:nx] = 15
+final[-1,nx:2*nx-2] = 5
+final[1:nx-1,-1] = 40
+final[0,nx:ny2-1] = 40
+final[0,ny2-1:-1] = 15
 fig=matshow(final)
 
 
